2919b381eee896e6667dc173d8ddca46f563d804
Chad Stearns <chad@superfocus.ai>
2025-06-27 18:29:39 -0400

    updated at nullable

--- db/migrations/2025-06-27-04:53:30____person-table.sql
+++ db/migrations/2025-06-27-04:53:30____person-table.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS person
     uuid       UUID PRIMARY KEY NOT NULL,
     name       TEXT             NOT NULL,
     created_at TIMESTAMP        NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP        NOT NULL DEFAULT NOW()
+    updated_at TIMESTAMP
 );
 
 COMMIT;
