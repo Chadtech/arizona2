b685e4ed124f97c78ebe708bbe56aab1b9196b06
Chad Stearns <chad@superfocus.ai>
2025-06-27 00:57:59 -0400

    Person table

---
+++ db/migrations/2025-06-27-04:53:30____person-table.sql
@@ -0,0 +1,13 @@
+-- person-table
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS person
+(
+    uuid       UUID PRIMARY KEY NOT NULL,
+    name       TEXT             NOT NULL,
+    created_at TIMESTAMP        NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP        NOT NULL DEFAULT NOW()
+);
+
+COMMIT;
