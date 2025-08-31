958e164dcdd7ae854be002e8c90f6fbb0b463eea
Chad Stearns <chad@superfocus.ai>
2025-08-31 10:44:57 -0400

    Scene capability and scene tables

--- src/capability/mod.rs
+++ src/capability/mod.rs
@@ -1,4 +1,5 @@
 pub mod memory;
 pub mod person;
 pub mod person_identity;
+mod scene;
 pub mod state_of_mind;
--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -2,4 +2,6 @@ pub mod memory_uuid;
 pub mod person_identity_uuid;
 pub mod person_name;
 pub mod person_uuid;
+pub mod scene_participant_uuid;
+pub mod scene_uuid;
 pub mod state_of_mind_uuid;
