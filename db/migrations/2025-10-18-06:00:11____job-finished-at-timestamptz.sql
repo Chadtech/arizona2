89bfb356561c007728754d72dc423026fc1cc6af
Chad Stearns <chad@superfocus.ai>
2025-11-02 19:06:00 -0500

    Set created at non-null default now

--- db/migrations/2025-10-18-06:00:11____job-finished-at-timestamptz.sql
+++ db/migrations/2025-10-18-06:00:11____job-finished-at-timestamptz.sql
@@ -7,6 +7,9 @@ ALTER TABLE job
     ALTER COLUMN created_at TYPE timestamptz
         USING (created_at AT TIME ZONE 'UTC'),
 
+    ALTER COLUMN created_at SET DEFAULT NOW(),
+    ALTER COLUMN created_at SET NOT NULL,
+
     ALTER COLUMN finished_at DROP DEFAULT,
     ALTER COLUMN finished_at TYPE timestamptz
         USING (finished_at AT TIME ZONE 'UTC'),
